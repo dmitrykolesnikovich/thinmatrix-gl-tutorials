fac6fb824a9c66baf9071f5732a60890850de9b1
Tristan Brindle <t.c.brindle@gmail.com>
2016-02-29 16:53:33 +1300

    Tutorial 20 done!
    That was easy
    
    https://www.youtube.com/watch?v=yGjMzIePKNQ

--- src/loader.cpp
+++ src/loader.cpp
@@ -91,8 +91,10 @@ loader::load_texture(const std::string& filename)
     glBindTexture(GL_TEXTURE_2D, texture_id);
     glTexImage2D(GL_TEXTURE_2D, 0, mode, w, h, 0, mode,
                  GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -0.4f);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
